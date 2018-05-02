149d5b5f26ae685dba47a3c451f2449e9b9685d8
HaiYang <haiyang@xiaohui.local>
2018-05-02 17:45:29 +0800

    branch test

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    branch test
     <img src="./assets/logo.png">
     <router-view/>
   </div>
@@ -13,7 +14,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
